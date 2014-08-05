19016df1107320832777cf1f2fef46d105423d3b
Prach Chaisatien <prach.c@gmail.com>
2014-08-05 23:18:46 +0900

    fix some file

--- src/song/MusicShuffle.java
+++ src/song/MusicShuffle.java
@@ -1,46 +1,33 @@
 package song;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
-
 
 public class MusicShuffle {
 	public static void main(String[] args) {
 		List<Song> songList = new ArrayList<Song>();
 		
-		songList.add(new Song("LUNA SEA","Shine","Time Has Come",1));
 		songList.add(new Song("LUNA SEA","Shine","Storm",2));
+		songList.add(new Song("LUNA SEA","Shine","Time Has Come",1));
 		songList.add(new Song("LUNA SEA","Shine","Shine",4));
 		
-		songList.add(new Song("LUNA SEA","Mother","Loveless",1));
 		songList.add(new Song("LUNA SEA","Mother","Rosier",2));
+		songList.add(new Song("LUNA SEA","Mother","Loveless",1));
 		songList.add(new Song("LUNA SEA","Mother","Mother",10));
 		
-		songList.add(new Song("ADTR","Homesick","The Downfall of Us All",1));
 		songList.add(new Song("ADTR","Homesick","NJ Legion Iced Tea",4));
+		songList.add(new Song("ADTR","Homesick","The Downfall of Us All",1));
 		songList.add(new Song("ADTR","Homesick","Homesick",8));
 		
 		songList.add(new Song("ADTR","Common Courtesy","Right Back at It Again",2));
 		songList.add(new Song("ADTR","Common Courtesy","I Surrender",9));
 		songList.add(new Song("ADTR","Common Courtesy","City of Ocala",1));
 		
-		Collections.sort(songList, new SongComparator());
-		
 		for(Song song:songList){
 			System.out.println(song.toString());
 		}
-		
-		System.out.println("//////");
-		
-		/*Collections.shuffle(songList);
-		
-		for(Song song:songList){
-			System.out.println(song.toString());
-		}*/
-		
-		System.out.println("//////");
 		
 		ArrayList<Song> shuffled = shuffle(songList);
 		
@@ -50,10 +37,11 @@ public class MusicShuffle {
 		}
 	}
 	
-	public static ArrayList<Song> shuffle(List<Song> sortedsonglist){
-		LinkedHashMap<String,ArrayList<Song>> albums = new LinkedHashMap<String,ArrayList<Song>>();
+	//O(n)
+	public static ArrayList<Song> shuffle(List<Song> anysonglist){
+		HashMap<String,ArrayList<Song>> albums = new HashMap<String,ArrayList<Song>>();
 		
-		for(Song song:sortedsonglist){
+		for(Song song:anysonglist){
 			String key = song.generateKey();
 			if(albums.get(key)==null){
 				ArrayList<Song> temp = new ArrayList<Song>();
@@ -72,13 +60,15 @@ public class MusicShuffle {
 		ArrayList<Song> result = new ArrayList<Song>();
 		
 		for(ArrayList<Song> songlist:albums2){
-			for(Song song:songlist){
-				System.out.println(song.toString());
-			}
+			Collections.sort(songlist, new SongComparator());
 			result.addAll(songlist);
-			System.out.println();
 		}
 		
 		return result;
 	}
+	
+	//Test
+	
+	//each album is in track order --> assert increment of each album
+	//album are really in random order --> compare key set
 }
--- src/song/SongComparator.java
+++ src/song/SongComparator.java
@@ -5,9 +5,8 @@ import java.util.Comparator;
 
 public class SongComparator implements Comparator<Song> {
     public int compare(Song a, Song b) {
-        int albumComparison = a.Album.compareTo(b.Album);
         Integer atracknum = a.TrackNum;
         Integer btracknum = b.TrackNum;
-        return albumComparison == 0 ? atracknum.compareTo(btracknum) : albumComparison;
+        return  atracknum.compareTo(btracknum);
     }
 }
